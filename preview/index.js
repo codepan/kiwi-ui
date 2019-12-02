f99f540cbaee27c3848a59b4361e9f76832d16d9
zhoupan <zhoupan@didiglobal.com>
2019-12-02 17:45:08 +0800

    remove comment

--- preview/index.js
+++ preview/index.js
@@ -8,20 +8,6 @@ import './assets/styles/index.css'
 /*开发环境全量测试*/
 import KiwiUI from '../src/index.js'
 Vue.use(KiwiUI)
-/*
-开发环境按需测试
-import List from '../components/list'
-import Item from '../components/item'
-Vue.use(List)
-Vue.use(Item)
-*/
-
-/*构建包全量测试
-import KiwiUI from '../lib'
-import '../lib/theme-default/index.css'
-*/
-
-// 构建包按需测试
 
 Vue.component(demoBlock.name, demoBlock)
 
